8367dcaee51f0cd7b7142b05f2ca7801bc7bc043
Hugo <hugo@variable.club>
2019-12-02 10:17:46 +0100

    responsive menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $('.open-image').on('click', function(){
     $(this).toggleClass('open-image-active');
 })
+
+$('.menu-button').on('click', function(){
+    $('.main').toggleClass('main--nav-active');
+})
+
+$('.nav a, .main--nav-active').on('click', function(){
+    $('.main').removeClass('main--nav-active');
+})
